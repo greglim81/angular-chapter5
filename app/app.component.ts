eda3b1747918cf20f9fab0130139994df92d3335
Greg Lim <limjunqu@gmail.com>
2017-04-17 09:55:36 +0800

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `    
-    <product-form></product-form>                            
+    <products></products>                            
     `
 })
 export class AppComponent {
