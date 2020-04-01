1a5a75a760c3d4a52a3ddbd3cecb47cc0fbf8024
Greg Lim <limjunqu@gmail.com>
2020-04-01 13:25:36 +0800

    Update products.component.ts

--- app/products.component.ts
+++ app/products.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core'
-import { ProductService} from './product.service'
+import { ProductService } from './product.service'
 
 @Component({
     selector: 'products',
     template: `
-            <h2>Products</h2>
-            <div *ngIf="products.length > 0">        
-                <div *ngFor="let product of products">
-                    <product [data]="product"></product>                
-                </div>
-            </div>
-            <div *ngIf="products.length == 0">
+        <h2>Products</h2>
+        <div *ngIf="products.length > 0; else loading">
+            <div *ngFor="let product of products">
+                <product [data]="product"></product>                
+            </div>  
+        </div>
+        <ng-template #loading>
+            <div>
                 No products to display                
             </div>
-        
-        `,
+        </ng-template>
+    `,
     providers: [ProductService]    
 })
 export class ProductsComponent{
@@ -23,4 +24,5 @@ export class ProductsComponent{
     constructor(productService: ProductService){
         this.products = productService.getProducts();
     }
+
 } 
