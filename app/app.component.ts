b6f0a4cba37c30403352f5820506ded4265320b5
Greg Lim <limjunqu@gmail.com>
2020-04-01 13:23:18 +0800

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `    
-    <products></products>                            
-    `
+  template: `  
+  <bs-jumbotron>
+    <div class="heading">
+      Hello World!
+    </div>    
+    <div class="body">
+      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+    </div>
+    <div class="description">
+      It uses utility classes for typography and spacing to space content out within the larger container.
+    </div>    
+    <div class="button">
+      Learn more
+    </div>      
+  </bs-jumbotron> 
+  `
 })
 export class AppComponent {
 }
-
-// <products></products>
